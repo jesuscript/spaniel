f125cdff59bded0b54efad89ac1334fcfce48721
KKudryavtsev <konstantin.k.ed@gmail.com>
2014-11-05 10:22:45 +0000

    making the node-client work...

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express"),
-    path = require("path");
+    path = require("path"),
+    Eth = require("./node-ethereum-tmp");
 
 var app = express();
 
@@ -20,3 +21,30 @@ app.get("/",function(req,res){
 var server = app.listen(app.get("port"), function(){
   console.log("Express server listening on port " + server.address().port);
 });
+
+
+var eth = new Eth({
+  network: {
+    host: "0.0.0.0",
+    port: 30303
+  },
+  rpc: true
+});
+
+eth.start(function(err){
+  if(err){
+    console.log("on start:", err);
+  } else {
+    console.log("Node ethereum started");
+  }
+
+  process.on('SIGTERM', shutdown);
+  process.on('SIGINT', shutdown);
+});
+
+function shutdown() {
+  eth.log.info('app', 'shutting down');
+  eth.stop(function () {
+    app.log.info('app', 'bye');
+  });
+}
